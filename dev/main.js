1fd4eb0c02ac1bd1d8aa9ed80aca726ae60c8614
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-20 16:56:58 +0200

    Test

--- dev/main.js
+++ dev/main.js
@@ -14,8 +14,8 @@ navigator.serviceWorker.register('./cache.js', {
 
   navigator.serviceWorker.addEventListener('message', function(evento) {
     alert("recived message from sw");
-    alert(evento);
-    /*$("#debugLogs").value = "evento.data"*/
+    alert(evento.data);
+    $("#debugLogs").value = "evento.data"
   });
 
 })
