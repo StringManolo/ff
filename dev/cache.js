8f62c2bcc5095d459514c27827241b6a2a041fff
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-20 16:42:44 +0200

    Test

--- dev/cache.js
+++ dev/cache.js
@@ -6,28 +6,28 @@ self.addEventListener('fetch', (event) => {
       message: 'The cache service worker write this text from 'fetch' event listener'
     });
   });
-  
+});
+/*  
   const destination = event.request.destination;
   switch (destination) {
     case 'style':
     case 'document': {
       event.respondWith(
-       /* debug.value = `style, or document: ${event.request}` */
+       
       );
       return;
     }
     case 'script': {
       event.respondWith(
-       /* debug.value = `script: ${event.request}`
- */     );
+  );
       return;
     }
     
     default: {
       event.respondWith(
-      /*  debug.value = `other resource: ${event.request}`*/
+      
       );
       return;
     }
-  }
-});
+  } 
+});*/
