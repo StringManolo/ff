aa3c1571981a5829d1d4f62afa8dc65b56ab3b02
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-22 14:56:33 +0200

    Try catch to debug

--- dev/main.js
+++ dev/main.js
@@ -202,4 +202,8 @@ ff.cache.resources = [
 "./resources/w3schoolspayload.png",
 "./resources/w3schoolsxsslanscape.png",
 "./resources/w3schoolsxss.png"];
-ff.cache.start("./cache.js", 100000);
+try {
+  ff.cache.start("./cache.js", 100000);
+} catch(err) {
+  alert("Error catched in main.js:\n"+err);
+}
